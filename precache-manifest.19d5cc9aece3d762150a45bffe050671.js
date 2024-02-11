2d4da293f667be9b37b629bf14c653a0fb17f329
sripwoud <me@sripwoud.xyz>
2024-02-11 22:23:20 +0100

    Updates

---
+++ precache-manifest.19d5cc9aece3d762150a45bffe050671.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93aed5d0de40e5279b0c7f4e2cf860cc",
+    "url": "/dj-covid/index.html"
+  },
+  {
+    "revision": "3a2071b89466c53e043c",
+    "url": "/dj-covid/static/js/2.cd15aa59.chunk.js"
+  },
+  {
+    "revision": "914a85462fc888825cb3a498c2b9869c",
+    "url": "/dj-covid/static/js/2.cd15aa59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "957c44c41e058014432e",
+    "url": "/dj-covid/static/js/main.e6added3.chunk.js"
+  },
+  {
+    "revision": "ac0f0549a9cd36fca30c",
+    "url": "/dj-covid/static/js/runtime-main.8265381a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.95f9022d9e56cede1d5175b48016a2c8.js"
+  "/dj-covid/precache-manifest.19d5cc9aece3d762150a45bffe050671.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dj-covid/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
